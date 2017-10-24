74557b26b29310474900dfa80a0da66306a5b373
Brad Farr <bradfarr@google.com>
2017-10-24 14:04:27 -0700

    added todo comments

--- src/main/java/com/codefellows/config/GcpConfigProvider.java
+++ src/main/java/com/codefellows/config/GcpConfigProvider.java
@@ -3,6 +3,7 @@ package com.codefellows.config;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class GcpConfigProvider implements IConfigProvider {
+    //TODO: Provide an implementation for the provide() method below.
     public Config provide(Class className) {
         throw new NotImplementedException();
     }
--- src/main/java/com/codefellows/config/SpringConfig.java
+++ src/main/java/com/codefellows/config/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+    //TODO: Alter the Spring configuration here so that the application uses GcpConfigProvider.
 
     @Bean
     public IConfigProvider configProvider() {
